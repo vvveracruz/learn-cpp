2257fbd1f61683deca0e22905da0956b5e721ea9
veracruz <ve.gomez@octoenergy.com>
2022-01-27 09:28:46 +0000

    Add new var declaration example and makefile.

---
+++ dawson2/ch1/game_stats.cpp
@@ -0,0 +1,22 @@
+// Game Stats
+// An example of initialising variables for a shooter game
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // types
+    int score;
+    double distance;
+    char playAgain;
+    bool shieldsUp;
+    short lives, aliensKilled;
+
+    // values
+    score = 0;
+    distance = 1200.76;
+    playAgain = 'y';
+    shieldsUp = true;
+    lives = 3;
+    aliensKilled = 10;
+}
