16f93ddaffb2fbc3cb92ecc69ec0a2cb220d507f
veracruz <ve.gomez@octoenergy.com>
2022-02-10 09:34:59 +0000

    update

---
+++ freecodecamp/hello_world.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello world!\n" << endl;
+    cout << "       /|" << endl;
+    cout << "      / |" << endl;
+    cout << "     /  |" << endl;
+    cout << "    /   |" << endl;
+    cout << "   /    |" << endl;
+    cout << "  /     |" << endl;
+    cout << " /      |" << endl;
+    cout << "/_______|\n" << endl;
+
+    string characterName = "John";
+    int characterAge = 35;
+
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was " << characterAge << " years old." << endl;
+
+    return 0;
+}
---
+++ freecodecamp/hello_world.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+g++ hello_world.cpp -o hello_world
+./hello_world
+rm hello_world
